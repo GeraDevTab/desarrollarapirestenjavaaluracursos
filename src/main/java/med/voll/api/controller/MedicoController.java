029f3f4891fdd21a6308d1160690a96070de58a9
Gerardo <everythingissofast@gmail.com>
2024-07-12 17:37:17 -0600

    implementacion actualizar, delete

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,13 +1,12 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DatosListadoMedico;
-import med.voll.api.medico.DatosRegistroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,9 +34,38 @@ public class MedicoController {
 //    }
 
     //el codigo siguiente es para tgrabajar con paginacion osea "pages"
-    @GetMapping
-    public Page<DatosListadoMedico> listadoMedicos(Pageable paginacion){
-        return medicoRepository.findAll(paginacion)
+    @GetMapping//la anotacion @pageabledefault, para establecer parametros por defecto, en este caso el size en 2
+    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 3) Pageable paginacion){
+//        return medicoRepository.findAll(paginacion)
+//                .map(DatosListadoMedico::new);
+        return medicoRepository.findByActivoTrue(paginacion)
                 .map(DatosListadoMedico::new);
     }
+
+    @PutMapping
+    @Transactional //@Transsactional esta anotacion es para asegurar que se ejecute la operacion enla base de datos,
+    // ademas que si tengo 2 o mas acciones a la base de datos y una falla, se hace un roll back a la
+    // base de datos y se regresa a su estado anterior, es decir, no sucedio nada.
+    public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosActualizarMedico){
+        Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
+        medico.actualizarDatos(datosActualizarMedico);
+
+    }
+
+    //en la sig linea, para que sea dinamico el dato, se le coloca entre llaves {id} para señalar que ahi llegaran
+    //datos variables
+    //con la anotacion @pathvariable es para que spring detecte que la variable id en este ejemplo va a venir de path variable
+    @DeleteMapping("/{id}")
+    @Transactional //si queremos que los datos sean cambiados y se ejecuten las consultas se le agrega @Transactional
+    public void eliminarMedico(@PathVariable Long id){
+        Medico medico = medicoRepository.getReferenceById(id);
+        medico.desactivarMedico();
+    }
+
+//  metodo que borra el registro en la base de datos
+//    public void eliminarMedico(@PathVariable Long id){
+//        Medico medico = medicoRepository.getReferenceById(id);
+//        //medicoRepository.delete(medico);
+//
+//    }
 }
--- src/main/java/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -1,7 +1,12 @@
 package med.voll.api.medico;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+
+import java.util.Optional;
 
 //al extender de JpaRepository se requiere el tipo de clase, y el tipo de dato del id, los dos deben ser indicados
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    Page<Medico> findByActivoTrue(Pageable paginacion);
 }
