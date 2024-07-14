4fb52beacfea2d2d028c092e4f63de892a5eaa10
Gerardo <everythingissofast@gmail.com>
2024-07-14 13:57:37 -0600

    creacion de TokenService y DTO Datos JwtToken

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,34 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuarios.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    public String generarToken(Usuario usuario){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("123456");
+            return JWT.create()
+                    //aqui en el string que se ingresa como parametro, es para indicar quien emite el token
+                    .withIssuer("voll med")
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
+                    .withExpiresAt(generarFechaExpiracion())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            // Invalid Signing configuration / Couldn't convert Claims.
+            throw new RuntimeException();
+        }
+    }
+
+    private Instant generarFechaExpiracion(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-06:00"));
+    }
+}
