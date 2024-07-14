f904dc6ae74836bd04f9e890a8a400b7a3620a81
Gerardo <everythingissofast@gmail.com>
2024-07-14 10:59:55 -0600

    clase securityconfiguration

--- src/main/java/med/voll/api/infra/security/AutenticacionService.java
+++ src/main/java/med/voll/api/infra/security/AutenticacionService.java
@@ -1,4 +1,20 @@
 package med.voll.api.infra.security;
 
-public class AutenticacionService {
+import med.voll.api.domain.usuarios.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service //con la interface UserDetailService se implementan los metos para mi autentication service
+public class AutenticacionService implements UserDetailsService {
+
+    //para fines de test no es recomendable poner aqaui mismo el autowired
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(username);
+    }
 }
