5b8130749ed704f39f615987d2f6f75e49c96e11
Gerardo <everythingissofast@gmail.com>
2024-07-14 09:49:13 -0600

    creacion clase usuario, dependencia spring security y migracion a base de datos usuario

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -1,10 +1,8 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
-
-import java.util.Optional;
 
 //al extender de JpaRepository se requiere el tipo de clase, y el tipo de dato del id, los dos deben ser indicados
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
---
+++ src/main/java/med/voll/api/infra/errores/TratadorDeErrores.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra.errores;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//con esta clase se personaliza en comportamiento de las respuestas de errores http
+//aqui digamos que tratamos los errores y personalizamos el mensaje devuelto hacia el cliente
+@RestControllerAdvice
+public class TratadorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e){
+        var errores = e.getFieldErrors().stream()
+                .map(DatosErrorValidacion::new).toList();
+        //con body ya no necesitamos el built
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    private record DatosErrorValidacion(String campo, String error){
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
