95c19e5bc5b8e3217ca373c461923b3e7d1413dd
Gerardo <everythingissofast@gmail.com>
2024-07-15 16:31:15 -0600

    implementacion de paciente clase, record, controller y respository

--- src/main/java/med/voll/api/domain/consulta/DatosDetallesConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DatosDetallesConsulta.java
@@ -1,8 +1,10 @@
-package med.voll.api.domain.consulta;
-
-import java.time.LocalDateTime;
-
-public class DatosDetallesConsulta {
-    public DatosDetallesConsulta(Long id, Long idPaciente, Long idMedico, LocalDateTime fecha) {
-    }
-}
+//package med.voll.api.domain.consulta;
+//
+//import java.time.LocalDateTime;
+//
+//public record DatosDetallesConsulta(Long id,
+//                                    Long idPaciente,
+//                                    Long idMedico,
+//                                    LocalDateTime fecha) {
+//
+//}
