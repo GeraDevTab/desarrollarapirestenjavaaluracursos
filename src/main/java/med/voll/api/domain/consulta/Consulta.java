7bd73619fdf74d27156123815c910c81d1d6bda8
Gerardo <everythingissofast@gmail.com>
2024-07-15 06:22:11 -0600

    creando clase consulta, dtoconsulta y controllerconsulta

---
+++ src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -0,0 +1,19 @@
+package med.voll.api.domain.consulta;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Consulta {
+    
+}
---
+++ src/main/java/med/voll/api/domain/consulta/DatosDetallesConsulta.java
@@ -0,0 +1,8 @@
+package med.voll.api.domain.consulta;
+
+import java.time.LocalDateTime;
+
+public class DatosDetallesConsulta {
+    public DatosDetallesConsulta(Long id, Long idPaciente, Long idMedico, LocalDateTime fecha) {
+    }
+}
