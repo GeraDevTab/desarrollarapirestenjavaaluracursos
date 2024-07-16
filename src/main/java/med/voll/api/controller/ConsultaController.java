7eb112de7416caddd2b921432250212cfebf5c85
Gerardo <everythingissofast@gmail.com>
2024-07-15 18:11:41 -0600

    validaciones de integridad en AgendaDeConsultaService

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,10 +2,13 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultaService;
 import med.voll.api.domain.consulta.DatosAgendarConsulta;
 import med.voll.api.domain.consulta.DatosDetallesConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +19,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendaDeConsultaService service;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DatosAgendarConsulta datos){
         System.out.println(datos);
 
+        service.agendar(datos);
         //*********
         //las lineas sig estarian violando el principio de responsabilidad unica asignandole
         //a nuestro controller que haga validaciones, lo ideal es crear un servicio que se encargue de eso AgendaDeConsultaService
